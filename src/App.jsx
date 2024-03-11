2915f9c7fd21563c4544bbde38838df4eb77d197
devmahin <mahinhowlader123@gmail.com>
2024-03-11 23:54:20 +0600

    setup ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-import { useState } from 'react'
 import './App.css'
-import Watch from './component/watch'
-import { useEffect } from 'react';
 
 function App() {
-  let [api,setApi] = useState([]);
-  useEffect(() => {
-    const apiFetch = async () => {
-      const apiJson = await fetch(`facebook.json`);
-      const apiConvat = await apiJson.json();
-      setApi(apiConvat)
-    }
+  
 
-    apiFetch()
-  }, [])
 
   return (
     <>
       
       <h1>Vite + React</h1>
-      {api.map(watch => <Watch key={watch.id} props={watch}></Watch>)}
     </> 
   )
 }
